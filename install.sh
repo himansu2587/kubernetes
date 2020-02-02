a9f654161a34912390207b242a5c23820caf1933
Ubuntu <ubuntu@ip-172-31-35-201.us-east-2.compute.internal>
2020-02-02 08:08:33 +0000

    k8s

---
+++ install.sh
@@ -0,0 +1,12 @@
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo apt-get remove docker docker-engine docker.io
+sudo apt install -y docker.io
+systemctl start docker
+systemctl enable docker
